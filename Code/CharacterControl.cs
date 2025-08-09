a84b8fd77262ac978a7b229d62bdf0d29097ad7a
Loupning <thelightningclan@gmail.com>
2025-08-09 19:14:04 -0400

    Character Movement + TempSprite

--- Code/CharacterControl.cs
+++ Code/CharacterControl.cs
@@ -1,6 +1,41 @@
 using Godot;
 using System;
 
-public partial class CharacterControl : Sprite2D
+public partial class CharacterControl : CharacterBody2D
 {
+    private int Speed = 32;
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        var _charDir = Input.GetVector("left", "right", "up", "down");
+
+        if (_charDir != Vector2.Zero)
+        {
+            GD.Print(_charDir.X);
+            var _char = GetNode<Sprite2D>("Character");
+            Velocity = _charDir * Speed;
+            if (_charDir.X == 1)
+            {
+                _char.RegionRect = new Rect2(0, 32, 32, 32); // character left sprite
+            }
+            else if (_charDir.X == -1)
+            {
+                _char.RegionRect = new Rect2(32, 32, 32, 32); // character right sprite
+            }
+            else if (_charDir.Y == 1)
+            {
+                _char.RegionRect = new Rect2(0, 0, 32, 32); // character up sprite
+            }
+             else if (_charDir.Y == -1)
+            {
+                _char.RegionRect = new Rect2(32, 0, 32, 32); // character down sprite
+            }
+            
+        }
+        else
+        {
+            Velocity = Vector2.Zero;
+        }
+        MoveAndSlide();
+    }
 }
