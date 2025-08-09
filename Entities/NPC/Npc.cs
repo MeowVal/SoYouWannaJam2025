8cf639d0e072ad7460342efcb55641ffb5405010
MeowVal <meowval@catkatnya.com>
2025-08-09 20:09:56 +0200

    npc stuff plus name fix

---
+++ Entities/NPC/Npc.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+namespace SoYouWANNAJam2025.Entities.NPC;
+
+public partial class Npc : RigidBody3D
+{
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+    
+}
