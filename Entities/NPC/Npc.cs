2cc67444ec61e1a40d7d610a57fa324f690edc72
MeowVal <meowval@catkatnya.com>
2025-08-09 20:18:35 +0200

    Rename to be consistent

--- Entities/NPC/Npc.cs
+++ Entities/NPC/Npc.cs
@@ -4,7 +4,11 @@ namespace SoYouWANNAJam2025.Entities.NPC;
 
 public partial class Npc : RigidBody3D
 {
-    
+    public Sprite3D sprite;
+    public override void _Ready()
+    {
+        sprite = GetNode<Sprite3D>("Sprite");
+    }
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
