48dc1717350f710981df9954a35d3dfb4e68e433
Loupning <thelightningclan@gmail.com>
2025-08-10 12:34:23 -0400

    Character Movement
    Restructured the movement script, added animation with temporary asset.

--- Code/CharacterControl.cs
+++ Code/CharacterControl.cs
@@ -13,7 +13,8 @@ public partial class CharacterControl : CharacterBody2D
 	public ModularWeapon HeldItem;
 
 	private double _currentSpeed;
-	private Sprite2D _charSprite;
+	private AnimatedSprite2D _charSprite;
+	private string _direction = "down";
 
 	public override void _Ready()
 	{
@@ -21,40 +22,80 @@ public partial class CharacterControl : CharacterBody2D
 		HeldItem = new ModularWeapon();
 		var resource = GD.Load<BaseWeaponModifier>("res://Resources/TestModifier.tres");
 		HeldItem.Modifiers.Add(resource);
-		_charSprite = GetNode<Sprite2D>("CharSprite");
+		_charSprite = GetNode<AnimatedSprite2D>("CharSprite");
 	}
 
-	public override void _PhysicsProcess(double delta)
+	private void PlayerMovement(double delta)
 	{
+		Vector2 velocity = Velocity;
+		//velocity.X = MoveSpeed;//, (float)(AccelerationSpeed);
+		//velocity.Y = (MoveSpeed / 2);//, (float)(AccelerationSpeed);
 		
-		var charDir = Input.GetVector("left", "right", "up", "down");
-		
-		if (charDir != Vector2.Zero)
+		if (Input.IsActionPressed("left"))
 		{
-			Velocity = Velocity.MoveToward(charDir * MoveSpeed, (float)(AccelerationSpeed));
+			velocity.X = -MoveSpeed;
+			velocity.Y = (MoveSpeed / 2);
+			Velocity = velocity;
 			
-			if (charDir.X == 1)
-			{
-				_charSprite.RegionRect = new Rect2(0, 32, 32, 32); // character left sprite
-			}
-			else if (charDir.X == -1)
-			{
-				_charSprite.RegionRect = new Rect2(32, 32, 32, 32); // character right sprite
-			}
-			else if (charDir.Y == 1)
-			{
-				_charSprite.RegionRect = new Rect2(0, 0, 32, 32); // character up sprite
-			}
-			else if (charDir.Y == -1)
-			{
-				_charSprite.RegionRect = new Rect2(32, 0, 32, 32); // character down sprite
-			}
-
-		} 
+			_direction = "left";
+			_charSprite.Play("RunSW");
+			//GD.Print("LEFT");
+		}
+		else if (Input.IsActionPressed("right"))
+		{
+			velocity.X = MoveSpeed;
+			velocity.Y = -(MoveSpeed / 2);
+			Velocity = velocity;
+			
+			_direction = "right";
+			_charSprite.Play("RunNE");
+			//GD.Print("right");
+		}
+		else if (Input.IsActionPressed("up"))
+		{
+			velocity.X = -MoveSpeed;
+			velocity.Y = -(MoveSpeed / 2);
+			Velocity = velocity;
+			
+			_direction = "up";
+			_charSprite.Play("RunNW");
+			//GD.Print("up");
+		}
+		else if (Input.IsActionPressed("down"))
+		{
+			velocity.X = MoveSpeed;
+			velocity.Y = (MoveSpeed / 2);
+			Velocity = velocity;
+			
+			_direction = "down";
+			_charSprite.Play("RunSE");
+			//GD.Print("down");
+		}
 		else
 		{
-			Velocity = Velocity.MoveToward(Vector2.Zero, (float)(DecelerationSpeed));
+			Velocity = velocity.MoveToward(Vector2.Zero, (float)(DecelerationSpeed)).Round();
+			switch (_direction)
+			{
+				case "right":
+					_charSprite.Play("idleNE");
+					break;
+				case "left":
+					_charSprite.Play("idleSW");
+					break;
+				case "up":
+					_charSprite.Play("idleNW");
+					break;
+				case "down":
+					_charSprite.Play("idleSE");
+					break;
+			}
 		}
+		
 		MoveAndSlide();
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		PlayerMovement(delta); //cleaned up _PhysicsProcess and moved player movement to its own function.
 	}
 }
