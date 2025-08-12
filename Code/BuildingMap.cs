82c8242e22ead00ccdbba2cf93fa8b023a233a38
MeowVal <meowval@catkatnya.com>
2025-08-12 02:02:06 +0200

    fixed obstacle avoidance on the Npc pathfinding
    you just need to add physics to a tile in a tileMap with a physics layer and add them to the group Obstacles and then have the BuildingMaps script on the tileMap with the navigation layer for the npc to pathfind aound them

--- Code/BuildingMap.cs
+++ Code/BuildingMap.cs
@@ -1,59 +1,60 @@
-using Godot;
-using System;
 using System.Collections.Generic;
+using Godot;
+
+namespace SoYouWANNAJam2025.Code;
 
 public partial class BuildingMap : TileMapLayer
 {
     private List<TileMapLayer> _obstacles = new List<TileMapLayer>();
 
-    public override void _Ready()
+    public override async void _Ready()
     {
-        GetObstaclesLayer();
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+        CallDeferred(nameof(NotifyRuntimeTileDataUpdate));
     }
-
-    private void GetObstaclesLayer()
-    {
-        var layers = GetTree().GetNodesInGroup("Obstacles");
-
-        foreach (var node in layers)
-        {
-            var layer = (Node2D)node;
-            if (layer is TileMapLayer mapLayer)
-            {
-                _obstacles.Add(mapLayer);
-                GD.Print("Obstacle Layer Added: " + mapLayer.Name);
-            }
-        }
-    }
-
     
     public override bool _UseTileDataRuntimeUpdate(Vector2I coords)
     {
-        return IsUsedByObstacles(coords);
+        return IsObstructedByObstacle(coords);
     }
-
-    private bool IsUsedByObstacles(Vector2I coords)
+    public override void _TileDataRuntimeUpdate(Vector2I coords, TileData tileData)
     {
-        foreach (var layer in _obstacles)
+        
+        if (IsObstructedByObstacle(coords))
         {
-            if (layer.GetUsedCells().Contains(coords))
+            tileData.SetNavigationPolygon(0, null); // Remove nav polygon for this tile
+        }
+    }
+    
+    private bool IsObstructedByObstacle(Vector2I coords)
+    {
+        Vector2 globalPos = MapToLocal(coords);
+
+        foreach (Node node in GetTree().GetNodesInGroup("Obstacles"))
+        {
+            if (node is TileMapLayer obstacleLayer)
             {
-                var isObstacle  = layer.GetCellTileData(coords).GetCollisionPolygonsCount(0) > 0;
-                if (isObstacle)
+                Vector2I localTileCoords = obstacleLayer.LocalToMap(globalPos);
+                int sourceId = obstacleLayer.GetCellSourceId(localTileCoords);
+                if (sourceId == -1)
+                    continue;
+
+                var tileSet = obstacleLayer.TileSet;
+                var source = tileSet.GetSource(sourceId);
+
+                if (source is TileSetAtlasSource atlasSource)
                 {
-                    return true; 
+                    var atlasCoords = obstacleLayer.GetCellAtlasCoords(localTileCoords);
+                    int alternative = obstacleLayer.GetCellAlternativeTile(localTileCoords);
+
+                    var tileData = atlasSource.GetTileData(atlasCoords, alternative);
+                    if (tileData != null && tileData.GetCollisionPolygonsCount(0) > 0)
+                    {
+                        return true;
+                    }
                 }
             }
         }
         return false;
-    }
-
-    public override void _TileDataRuntimeUpdate(Vector2I coords, TileData tileData)
-    {
-        
-        if (!IsUsedByObstacles(coords))
-        {
-            tileData.SetNavigationPolygon(0, null);
-        }
     }
 }
---
+++ Code/Npc/NpcInteractor.cs
@@ -0,0 +1,81 @@
+using Godot;
+using Godot.Collections;
+
+namespace SoYouWANNAJam2025.Code.Npc;
+
+public partial class NpcInteractor : Area2D
+{
+    public Array<Interactible> PossibleTargets = [];
+    public int CurrentTarget = 0;
+    private Timer _distanceCheckTimer;
+    private CharacterControl _player;
+
+    public InventorySlot InventorySlot = null;
+    public override void _Ready()
+    {
+        if (GetParent().FindChild("InventorySlot") is InventorySlot slot) InventorySlot=slot;
+        
+        _distanceCheckTimer = GetNode<Timer>("./DistanceCheckTimer");
+        _distanceCheckTimer.Timeout += OnDistanceCheckTimer;
+        _player = GetParent<CharacterControl>();
+
+        BodyEntered += OnInteractableEntered;
+        BodyExited += OnInteractableExited;
+        AreaEntered += OnInteractableEntered;
+        AreaExited += OnInteractableExited;
+    }
+
+    private void OnDistanceCheckTimer()
+    {
+        if (PossibleTargets.Count == 1)
+        {
+            PossibleTargets[0].SetHighlight(true);
+            return;
+        }
+
+        int nearestTarget = 0;
+        float nearestDistance = float.MaxValue;
+
+        for (var i = 0; i < PossibleTargets.Count; i++)
+        {
+            var distance = PossibleTargets[i].GlobalPosition.DistanceTo(_player.GlobalPosition);
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestTarget = i;
+            }
+        }
+
+        CurrentTarget = nearestTarget;
+        for (var i = 0; i < PossibleTargets.Count; i++)
+        {
+            PossibleTargets[i].SetHighlight(i == CurrentTarget);
+        }
+    }
+    private void OnInteractableEntered(Node2D unknownTarget)
+    {
+        if (unknownTarget is not Interactible target) return;
+        PossibleTargets.Add(target);
+        OnDistanceCheckTimer();
+    }
+
+    private void OnInteractableExited(Node2D unknownTarget)
+    {
+        if (unknownTarget is not Interactible target) return;
+        PossibleTargets.Remove(target);
+        target.SetHighlight(false);
+        OnDistanceCheckTimer();
+    }
+
+    public void TriggerInteraction()
+    {
+        if (PossibleTargets.Count > 0 && CurrentTarget < PossibleTargets.Count)
+        {
+            PossibleTargets[CurrentTarget].TriggerInteraction(this);
+        }
+        else if (InventorySlot.Item != null)
+        {
+            InventorySlot.DropItem(GetParent().GetParent(), GlobalPosition);
+        }
+    }
+}
