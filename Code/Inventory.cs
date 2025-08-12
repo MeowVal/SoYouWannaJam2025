974e9802dd3cb02f3349c9002c2664994a4a695f
Davemane42 <davemane43@gmail.com>
2025-08-12 05:51:58 -0400

    More robust inventory + multi item crafting

---
+++ Code/Inventory.cs
@@ -0,0 +1,131 @@
+using System.Linq;
+using Godot;
+using Godot.Collections;
+using SoYouWANNAJam2025.Code.Items;
+
+namespace SoYouWANNAJam2025.Code;
+
+public partial class Inventory : InventorySlot
+{
+    public Array<InventorySlot> Slots;
+    private int _lastSlot = 0;
+    private int _count = 0;
+
+    private void CalculateCount()
+    {
+        _count = Slots.Count(slot => slot.HasItem());
+    }
+
+    private int FindSlot(bool empty = true, bool reversed = false)
+    {
+        for (var i = 0; i < Slots.Count; i++)
+        {
+            var x = reversed ?  Slots.Count-1-i: i;
+            if ((Slots[x].HasSpace() && empty) || (Slots[x].HasItem() && !empty)) return x;
+        }
+
+        return -1;
+    }
+    
+    public override void _Ready()
+    {
+        Slots = new Array<InventorySlot>(GetChildren()
+            .Where(child => child is InventorySlot)
+            .Select(child => child)
+            .Cast<InventorySlot>()
+        );
+    }
+    
+    public override void CompileWhitelist()
+    {
+        base.CompileWhitelist();
+        foreach (var slot in Slots)
+        {
+            slot.RecipeWhitelist = RecipeWhitelist;
+            slot.CompileWhitelist();
+        }
+    }
+    
+    // Moves item from one slot directly to another, returns true if successful.
+    public override bool TransferTo(InventorySlot slot)
+    {
+        if (!HasItem() || !slot.HasSpace()) return false;
+        
+        if (!Slots[_lastSlot].TransferTo(slot)) return false;
+        CalculateCount();
+        _lastSlot = FindSlot(empty:false, reversed: true);
+        GD.Print("Inventory transfer");
+        return true;
+    }
+
+    // Add item to the slot, returns true if successful.
+    public override bool PickupItem(GenericItem item, bool forceAdd = false)
+    {
+        if (!HasSpace()) return false;
+        var index = FindSlot();
+        if (index == -1 || !Slots[index].PickupItem(item, forceAdd)) return false;
+        _lastSlot = index;
+        CalculateCount();
+        return true;
+    }
+
+    // Removes item from the slot, returns true if successful.
+    public override bool DropItem(Node parentNode, Vector2 globalPosition)
+    {
+        if (!HasItem()) return false;
+        if (!Slots[_lastSlot].DropItem(parentNode, globalPosition)) return false;
+        CalculateCount();
+        _lastSlot = FindSlot(empty:false, reversed: true);
+        return true;
+    }
+    
+    public override bool DestroyItem(Array<BaseItem> items)
+    {
+        if (!HasItem() || items.Count > _count || !ContainItem(items, true)) return false;
+        
+        var dupeItems = new Array<BaseItem>(items);
+        foreach (var slot in Slots)
+        {
+            if (slot.HasItem())
+            {
+                var slotItemResource = slot.Item.ItemResource;
+                if (slot.DestroyItem(dupeItems)) dupeItems.Remove(slotItemResource);
+            }
+            
+        }
+        CalculateCount();
+        _lastSlot = FindSlot(reversed: true);
+
+        return true;
+    }
+    
+    public override bool ContainItem(Array<BaseItem> items, bool all = false)
+    {
+        if (!HasItem() || (all && items.Count > _count)) return false;
+        if (!all) return Slots.Any(slot => slot.ContainItem(items));
+
+        var dupeItems = new Array<BaseItem>(items);
+        foreach (var slot in Slots)
+        {
+            if (slot.ContainItem(dupeItems))
+            {
+                dupeItems.Remove(slot.Item.ItemResource);
+            }
+        }
+
+        return dupeItems.Count == 0;
+
+    }
+
+    public override bool HasItem()
+    {
+        return _count != 0;
+    }
+    
+    public override bool HasSpace()
+    {
+        return _count < Slots.Count;
+    }
+    
+    
+}
