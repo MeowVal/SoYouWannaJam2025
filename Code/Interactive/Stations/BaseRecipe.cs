a558078db05a65035bf84c572a6873523f63c90d
Ramiris <sirchopwood@gmail.com>
2025-08-14 05:07:50 +0100

    Re-order recipe exports

--- Code/Interactive/Stations/BaseRecipe.cs
+++ Code/Interactive/Stations/BaseRecipe.cs
@@ -29,11 +29,14 @@ public partial class BaseRecipe : Resource
     [Export] public string Description = "You need to actually set this lol.";
     [Export] public Texture2D Icon;
 
-    [ExportGroup("Recipe Data")]
-    [Export] public EWorkType WorkType = EWorkType.Instant;
-    [Export] public float TimeToComplete = 1.5f;
+    [ExportGroup("Inputs/Outputs")]
     [Export] public ERecipeType RecipeType = ERecipeType.Standard;
     [Export] public Array<GenericItemTemplate> ItemInputs = [];
     [Export] public Array<GenericItemTemplate> ItemOutputs = [];
     [Export] public ModularPartTemplate PartOutput;
+
+    [ExportGroup("Minigame")]
+    [Export] public EWorkType WorkType = EWorkType.Instant;
+    [Export] public float TimeToComplete = 1.5f;
+    [Export] public int SequenceLength = 6;
 }
